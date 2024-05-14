14606b53f1d69baf6cba30156dad0cec1cfc9a41
Will Arachelian <william.arachelian@gmail.com>
2024-05-14 18:46:39 -0400

    completed

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+
+const connection = new MongoClient("mongodb://localhost:27017/");
+const db = connection.db("url_shortener");
+const urls = db.collection("urls");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -15,9 +25,35 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', async function(req, res) {
+  try {
+    let url = new URL(req.body.url);
+    
+    let search = await urls.findOne({original_url: url.href});
+    if (search) {
+      return res.json({original_url: search.original_url, short_url: search.short_url}); 
+    }
+
+    let dbcount = await urls.countDocuments();
+    let newUrl = {original_url: url.href, short_url: dbcount};
+    let results = await urls.insertOne(newUrl);
+
+    if(!results) throw "Insert Failed";
+    return res.json({original_url: newUrl.original_url, short_url: newUrl.short_url});
+     
+  } catch(e) {
+    return res.json({error: "invalid url"})
+  }
 });
+app.get('/api/shorturl/:shorturl', async function(req, res) {
+  console.log(req.params.shorturl)
+
+
+  let urlObj = await urls.findOne({short_url: parseInt(req.params.shorturl)});
+  console.log(urlObj);
+  return res.redirect(urlObj.original_url);
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
